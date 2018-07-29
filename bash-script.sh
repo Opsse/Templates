205e42e636bf128d49d168ef6f2cf3c7aac35c88
Victor Yon <contact@victor-yon.fr>
2018-07-29 16:22:59 +0200

    add first bash script template

---
+++ bash-script.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Version 1.0
+
+# Font
+RED="$(tput setaf 1)"
+GREEN="$(tput setaf 2)"
+YELLOW="$(tput setaf 3)"
+BLUE="$(tput setaf 4)"
+MAGENTA="$(tput setaf 5)"
+CYAN="$(tput setaf 6)"
+WHITE="$(tput setaf 7)"
+BOLD="$(tput bold)"
+UNDER="$(tput smul)"
+OFF="$(tput sgr0)"
+
+echo "${CYAN}${BOLD}TODO${OFF}"
+
+exit 0
